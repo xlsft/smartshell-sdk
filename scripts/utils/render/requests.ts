b62b1d4a8bc6e693fa2cf64f17db8af88868371c
XL Software <dev@xlsoftware.ru>
2024-06-04 21:49:37 +0300

    chore: :memo: save

--- scripts/utils/render/requests.ts
+++ scripts/utils/render/requests.ts
@@ -1,10 +1,10 @@
 import type { Field, Method, Type, TypeRef } from "../../types/types.ts";
-import ts from "npm:typescript";
 
 const scalars = [{ name: 'String', type: 'string' }, { name: 'Int', type: 'number' }, { name: 'Float', type: 'number' }, { name: 'Boolean', type: 'boolean'}]
-const deadend = ['Upload', 'Email', 'IpAddress', 'MacAddress', 'Date', 'Time', 'DateTime']
+const defaults = ['Upload', 'Email', 'IpAddress', 'MacAddress', 'Date', 'Time', 'DateTime']
+const natives = ['string', 'number', 'boolean']
 
-const method = (type: 'query' | 'string', method: Method, types: Type[] ) => {
+const method = (type: 'query' | 'string', method: Method, _types: Type[] ) => {
     const resolve = (type: TypeRef) => {
         const options = { required: false, array: false, value: '' }
         const recursive = (type: TypeRef) => {
@@ -24,94 +24,87 @@ const method = (type: 'query' | 'string', method: Method, types: Type[] ) => {
     const paginated = method.args.some(arg => arg.name === 'page' || arg.name === 'first'); if (paginated) { imports.sdk.add('ShellSdkPaginatorInput'); props.push({ key: 'paginator', value: 'ShellSdkPaginatorInput', required: false, array: false }) }
     const prop = (arg: Field) => { if (arg.name === 'page' || arg.name === 'first') return; const resolved = resolve(arg.type); props.push({ key: arg.name, ...resolved }) }; method.args.forEach(arg => prop(arg))
         const structure = (name: string) => {
+
+            type Node = { key: string, value: string[], required: boolean, array: boolean, scalar: boolean, child?: Node[] | Child }
+            type Child = { type: 'union' | 'enum', value: string[] }
+
             const file = Deno.readTextFileSync('src/types/types.ts')
-            // Херня, должно искать из всего файла и возвращать тру и фолс
-            const object_type_regex = /export\s+type\s+\w+\s*=\s*\{[^}]*\}/;
-            const union_type_regex = /export\s+type\s+\w+\s*=\s*[^{}]*(&\s*\w+\s*)+/;
-            const enum_type_regex = /export\s+type\s+\w+\s*=\s*(("[^"]*"\s*\|\s*)*("[^"]*"))/;
+
+            const is = {
+                object: (name: string): boolean => {
+                    const object_regex = new RegExp(`export type ${name} = {[^}]+}`, 's');
+                    return object_regex.test(file);
+                },
+                enumerated: (name: string): boolean => {
+                    const enum_regex = new RegExp(`export type ${name} = ("[^"]+"(?: \\| "[^"]+")*)`);
+                    const match = file.match(enum_regex);
+                    if (!match) return false;
+                    const enumerated = match[1].split('|').map(v => v.trim());
+                    return enumerated.every(v => v.startsWith('"') && v.endsWith('"'));
+                },
+                union: (name: string): boolean => {
+                    const union_regex = new RegExp(`export type ${name} = ([^\\n]+)`);
+                    const match = file.match(union_regex);
+                    if (!match) return false;
+                    const union = match[1].split('|').map(v => v.trim());
+                    // Проверка на то, что ни одно из значений не является строковым литералом
+                    return union.some(v => !v.startsWith('"') && !v.endsWith('"')) && !is.object(name);
+                }    
+            };
 
             const node = (name: string) => {
-                if (deadend.includes(name) || ['string', 'number', 'boolean'].includes(name)) return name
-                if (deadend.includes(name) && !imports.gql.has(name)) imports.gql.add(name);
+                if (defaults.includes(name) && !imports.gql.has(name)) imports.gql.add(name);
+ 
+                const object = (): Node[] => {
+                    const array = file.split(`export type ${name} = {`)[1].split('}')[0].trim().split('\n').map(o => o.trim());
+                    const result = array.map(raw_node => {
+                        const [raw_key, raw_value] = raw_node.split(':').map(part => part.trim());
+                        return {
+                            key: raw_key.endsWith('?') ? raw_key.slice(0, -1) : raw_key,
+                            value: [raw_value.endsWith('[]') ? raw_value.slice(0, -2) : raw_value],
+                            required: !raw_key.endsWith('?'),
+                            array: raw_value.endsWith('[]'),
+                            scalar: [...natives, ...defaults].includes(raw_value.endsWith('[]') ? raw_value.slice(0, -2) : raw_value),
+                        }
+                    })
+                    return result
+                }
+
+                const enumerated = (): Child => {
+                    return { type: 'enum', value: file.split(`export type ${name} = `)[1].split('\n')[0].split(' | ').map(v => v.split('').map(iv => iv === "\"" ? '' : iv).join('') ) }
+                }
+
+                const union = (): Child => {
+                    return { type: 'union', value: file.split(`export type ${name} = `)[1].split('\n')[0].split(' | ').map(v => v.split('').map(iv => iv === "\"" ? '' : iv).join('') ) }
+                }
                 
-                
-                const node = file.split(`export type ${name} = {`)[1].split('}')[0].trim().split('\n').map(o => o.trim())
+                if (is.object(name)) return object()
+                else if (is.enumerated(name)) return enumerated()
+                else if (is.union(name)) return union()
+            } 
 
-
-
-                console.log(node)
+            const parent = node(name)!
+            const result = []
+            let level = 0
+            //@ts-ignore <i dont care>
+            for (let i = 0; i < parent.length; i++) {
+                //@ts-ignore <i REALLY dont care>
+                const item: Node = parent![i];
+                const paginated = item.key === "paginatorInfo"
+                if (paginated) continue
+                result.push(item)
             }
+            for (let i = 0; i < result.length; i++) {
+                // Root
+                const item = result[i];
+                level++
+                if (item.scalar === true) continue
+                else {
+                    
+                }
+            }
+            
 
-            console.log(node('CompanyPermissions'))
-
-
-            // console.log(source.statements[0])
-            // const getTypeNode = (typeName: string): ts.TypeNode | null => {
-            //     for (const statement of sourceFile.statements) {
-            //         if (ts.isTypeAliasDeclaration(statement) && (statement.name as ts.Identifier).text === typeName) {
-            //             return statement.type;
-            //         }
-            //         if (ts.isInterfaceDeclaration(statement) && (statement.name as ts.Identifier).text === typeName) {
-            //             const symbol = checker.getSymbolAtLocation(statement.name);
-            //             if (symbol) {
-            //                 const declaration = symbol.declarations[0];
-            //                 if (ts.isInterfaceDeclaration(declaration)) {
-            //                     return declaration;
-            //                 }
-            //             }
-            //         }
-            //     }
-            //     return null;
-            // };
-    
-            // const getTypeString = (node: ts.TypeNode, level = 0, visited = new Set<ts.TypeNode>()): string => {
-            //     if (visited.has(node)) {
-            //         return ''; // Avoid infinite recursion for cyclic references
-            //     }
-            //     visited.add(node);
-    
-            //     if (ts.isUnionTypeNode(node)) {
-            //         const types = node.types.map(typeNode => getTypeString(typeNode, level + 1, visited)).filter(Boolean);
-            //         return `on('entity', [${types.join(', ')}])`;
-            //     } else if (ts.isTypeLiteralNode(node) || ts.isInterfaceDeclaration(node)) {
-            //         const members = ts.isTypeLiteralNode(node) ? node.members : (node as ts.InterfaceDeclaration).members;
-            //         const fields = members.map(member => {
-            //             if (ts.isPropertySignature(member)) {
-            //                 const name = (member.name as ts.Identifier).text;
-            //                 if (name === 'paginatorInfo') return ''; // Skip paginatorInfo
-            //                 const typeNode = member.type;
-            //                 if (typeNode) {
-            //                     const typeStr = getTypeString(typeNode, level + 1, visited);
-            //                     if (typeStr) {
-            //                         return deadend.includes(typeStr.replace(/\"/g, "")) ? `"${name}"` : `key("${name}", ${typeStr})`;
-            //                     }
-            //                     return `"${name}"`;
-            //                 }
-            //             }
-            //             return '';
-            //         }).filter(Boolean);
-            //         return `[${fields.join(', ')}]`;
-            //     } else if (ts.isTypeReferenceNode(node)) {
-            //         const typeName = (node.typeName as ts.Identifier).text;
-            //         if (deadend.includes(typeName) || scalars.some(s => s.name === typeName)) {
-            //             return `"${typeName}"`;
-            //         }
-            //         const referencedTypeNode = getTypeNode(typeName);
-            //         if (referencedTypeNode) {
-            //             return getTypeString(referencedTypeNode, level, visited);
-            //         }
-            //         return '';
-            //     } else if (ts.isArrayTypeNode(node)) {
-            //         return getTypeString(node.elementType, level, visited);
-            //     }
-            //     return '';
-            // };
-    
-            // const rootTypeNode = getTypeNode(name);
-            // if (rootTypeNode) {
-            //     return getTypeString(rootTypeNode);
-            // }
-            // return '';
         };
         
     return {
@@ -129,49 +122,4 @@ const method = (type: 'query' | 'string', method: Method, types: Type[] ) => {
 
 export const modules = (query: Method[], mutations: Method[], types: Type[]) => {
     query.forEach(item => {if (item.name === 'clients') console.log(method('query', item, types).structure)})
-    // const deadend_types = ['Upload', 'Email', 'IpAddress', 'MacAddress', 'Date', 'Time', 'DateTime']
-    // const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
-    // const result: Request[] = []
-    // let query: Type | null = null
-    // let mutation: Type | null = null
-    // for (let i = 0; i < introspect.length; i++) {
-    //     const type = introspect[i];
-    //     if (type.name === 'Query') query = type
-    //     else if (type.name === 'Mutation') mutation = type
-    //     else continue
-    // }
-    // const rendered_types = Deno.readTextFileSync('src/types/types.ts')
-
-    // for (let top_type_i = 0; top_type_i < introspect.length; top_type_i++) {
-    //     const type = introspect[top_type_i]
-    //     if ( type.kind === 'UNION' ) console.log(type)
-    // }
-
-    
-
-    // for (let i = 0; i < query!.fields.length; i++) {
-    //     const request = query?.fields[i];
-    //     const extracted_type_start = rendered_types.split(`export type ${capitalize(request!.name!)} = {\n`)[1]
-    //     if (extracted_type_start === undefined) continue
-    //     const extracted_types = extracted_type_start.split('}')[0].split('\n').map((type) => { return type.trim() })
-    //     extracted_types.pop()
-    //     const top_fields: RequestField[] = []
-    //     for (let j = 0; j < extracted_types.length; j++) {
-    //         const top_field = extracted_types[j];
-    //         const [top_field_name, top_field_type] = top_field.split('?').join('').split(': ')
-
-    //         const top_field_has_nested = /^[A-Z]/.test(top_field_type) && !deadend_types.includes(top_field_type)
-
-    //         // console.log(top_field_name, top_field_type, top_field_has_nested)
-    //     }
-    // }
-    
-    // console.log(query, mutation)
-
-    
-    // result.push({
-    //     type: 'query',
-    //     name: request!.name,
-    //     input:
-    // })
 }
